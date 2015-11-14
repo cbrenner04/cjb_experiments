12abd1d05040214b049d9a6e08284e9097698b31
cbrenner04 <christopher.brenner1@northwestern.edu>
2015-11-13 23:56:59 -0600

    Updates

---
+++ if_test.rb
@@ -0,0 +1,49 @@
+require 'benchmark'
+
+def if_1
+  Benchmark.bm(7) do |x|
+    x.report('if >:') do
+      x = 2
+      1_000_000_000.times do
+        puts 'x < 1' if x < 1
+      end
+    end
+  end
+end
+
+def unless_1
+  Benchmark.bm(7) do |x|
+    x.report('unless >:') do
+      x = 2
+      1_000_000_000.times do
+        puts 'x < 1' unless x > 1
+      end
+    end
+  end
+end
+
+def if_2
+  Benchmark.bm(7) do |x|
+    x.report('if !=:') do
+      x = 2
+      1_000_000_000.times do
+        puts 'x != 2' if x != 2
+      end
+    end
+  end
+end
+
+def unless_2
+  Benchmark.bm(7) do |x|
+    x.report('unless ==:') do
+      x = 2
+      1_000_000_000.times do
+        puts 'x != 2' unless x == 2
+      end
+    end
+  end
+end
+
+200.times do
+  send %i(if_1 unless_1 if_2 unless_2).sample
+end
--- while_test.rb
+++ while_test.rb
@@ -4,7 +4,7 @@ def while_loop_1
   Benchmark.bm(7) do |x|
     x.report('while <:') do
       i = 0
-      i += 1 while i < 1000000000
+      i += 1 while i < 1_000_000_000
     end
   end
 end
@@ -13,7 +13,7 @@ def while_loop_2
   Benchmark.bm(7) do |x|
     x.report('while !=:') do
       i = 0
-      i += 1 while i != 1000000000
+      i += 1 while i != 1_000_000_000
     end
   end
 end
@@ -22,7 +22,7 @@ def until_loop_1
   Benchmark.bm(7) do |x|
     x.report('until ==:') do
       i = 0
-      i += 1 until i == 1000000000
+      i += 1 until i == 1_000_000_000
     end
   end
 end
@@ -31,7 +31,7 @@ def until_loop_2
   Benchmark.bm(7) do |x|
     x.report('until >:') do
       i = 0
-      i += 1 until i > 1000000000
+      i += 1 until i > 1_000_000_000
     end
   end
 end
